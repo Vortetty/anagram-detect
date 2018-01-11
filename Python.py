be2e978324800746de3cc2e3dcc2b24ce31ae6dc
Vortetty <33466216+Vortetty@users.noreply.github.com>
2018-01-11 08:52:10 -0800

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,1 +1,10 @@
-input(
+x = input("what would you like to check for anagrams")
+y = x[::-1]
+ if y == x:
+	print("you have an anagram on your hands")
+elif y!=x:
+	print("sorry, no anagram here")
+else:
+	print("woops, looks like somthing broke, please try again later or reload the page")
+	
+print("\nmade by vortetty for mohan")
